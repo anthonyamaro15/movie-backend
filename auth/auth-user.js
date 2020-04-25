e2f0f473e6063d0a237c2a335e5e7f77de19ee10
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-04-25 00:25:13 -0700

    able to get favorite movies for authorizated user

--- auth/auth-user.js
+++ auth/auth-user.js
@@ -1,9 +1,54 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { jwtSecret } = require("../config/secret");
+
+const User = require("../user-schema/user-model");
 
 const route = express.Router();
 
-route.get("/", (req, res) => {
-  res.json("hello");
+route.post("/register", (req, res) => {
+  const userInfo = req.body;
+
+  const hash = bcrypt.hashSync(userInfo.password, 8);
+  userInfo.password = hash;
+  User.add(userInfo)
+    .then((user) => {
+      res.status(201).json(user);
+    })
+    .catch(({ error, message }) => {
+      res.status(400).json({ error, message });
+    });
 });
+
+route.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  User.findBy({ email })
+    .then((user) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: user.username,
+          token,
+        });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(({ error, message }) => {
+      res.status(500).json({ error, message });
+    });
+});
+
+function generateToken(user) {
+  const payload = {
+    user: user.username,
+  };
+  const options = {
+    expiresIn: "1h",
+  };
+  return jwt.sign(payload, jwtSecret, options);
+}
 
 module.exports = route;
--- user-schema/userRouter.js
+++ user-schema/userRouter.js
@@ -1,9 +1,28 @@
 const express = require("express");
+const User = require("./user-model");
 
 const route = express.Router();
 
-// route.get('/', (req, res) => {
-//    res.js
-// })
+route.get("/", (req, res) => {
+  console.log("does it runs?");
+  User.find()
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch(({ error, message }) => {
+      res.status(400).json({ error, message });
+    });
+});
+
+route.get("/favorites/:id", (req, res) => {
+  const { id } = req.params;
+  User.getFavMovies(id)
+    .then((movie) => {
+      res.status(200).json(movie);
+    })
+    .catch(({ error, message }) => {
+      res.status(404).json({ error, message });
+    });
+});
 
 module.exports = route;
