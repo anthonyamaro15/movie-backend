742c611e7b34e6ea3897df8f2f0f2e2635df1a7c
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-04-26 01:31:35 -0700

    authenticated user id, and check if email already exists in database

--- user-schema/user-model.js
+++ user-schema/user-model.js
@@ -8,6 +8,16 @@ function findBy(email) {
   return db("users").where(email).first();
 }
 
+function findById(id) {
+  return db("users").where({ id }).first();
+}
+
+function findFavById(user_id) {
+  return db("users as u")
+    .join("favorite as f", "u.id", "f.user_id")
+    .where({ user_id });
+}
+
 function find() {
   return db("users");
 }
@@ -19,18 +29,20 @@ function getFavMovies(id) {
     .select("f.id", "f.favorite_movie");
 }
 
-// function removeFav(id) {
-//   return db("users as u")
-//     .join("favorite as f", "u.id", "f.user_id")
-//     .where("u.id", id)
-//     .select('f.favorite_movie')
-//     .del();
-// }
+function removeFav(id) {
+  return db("users as u")
+    .join("favorite as f", "u.id", "f.user_id")
+    .where("f.id", id)
+    .select("f.favorite_movie")
+    .del();
+}
 
 module.exports = {
   add,
   findBy,
   find,
   getFavMovies,
-  //   removeFav,
+  findById,
+  findFavById,
+  removeFav,
 };
