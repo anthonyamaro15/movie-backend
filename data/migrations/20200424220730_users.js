7816ee60a8ab281b82bb4487663b498008b1002f
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-05-16 17:44:55 -0700

    adding new seeds

--- data/migrations/20200424220730_users.js
+++ data/migrations/20200424220730_users.js
@@ -2,17 +2,43 @@ exports.up = function (knex) {
   return knex.schema
     .createTable("users", (table) => {
       table.increments();
-      table.string("email", 128).unique("email").notNullable();
+      table.string("email", 128).unique("email").notNullable().unique();
       table.string("username", 128).unique("username").notNullable();
       table.string("password", 128).notNullable();
     })
     .createTable("favorite", (table) => {
       table.increments();
-      table.string("favorite_movie");
-      table.integer("user_id").notNullable().unsigned().references("users.id");
+      table.string("backdrop_path", 255).notNullable();
+      table.integer("id").notNullable();
+      table.string("original_language", 255).notNullable();
+      table.string("original_title", 255);
+      table.float("popularity");
+      table.string("overview", 255);
+      table.string("poster_path", 255);
+      table.string("title", 255);
+      table.float("vote_average");
+      table.integer("vote_count");
+    })
+    .createTable("user_movie", (table) => {
+      table.increments();
+      table
+        .integer("movie_id")
+        .unsigned()
+        .references("favorite.f_id")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      table
+        .integer("users_id")
+        .unsigned()
+        .references("users.id")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
     });
 };
 
 exports.down = function (knex) {
-  return knex.schema.dropTableIfExists("favorite").dropTableIfExists("users");
+  return knex.schema
+    .dropTableIfExists("user_movie")
+    .dropTableIfExists("favorite")
+    .dropTableIfExists("users");
 };
